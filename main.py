7e29cd2b35653309e9d1a892d0ad710b7960dc92
AlexP04 <84740734+AlexP04@users.noreply.github.com>
2022-11-19 13:05:35 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -6,3 +6,13 @@ st.set_page_config(page_title='СА ЛР2',
                    menu_items={
                        'About': 'Лабораторна робота №2 з системного аналізу'
                    })
+
+st.markdown("""
+    <style>
+    .stProgress .st-ey {
+        background-color: #5fe0de;
+    }
+    </style>
+    """, unsafe_allow_html=True)
+
+st.title('Select parameters and run: ')
