28e20986bf35f017d39165027b8a559dd81d784f
AlexP04 <84740734+AlexP04@users.noreply.github.com>
2022-11-19 21:54:06 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ if main.button('Run', key='run'):
         #Parsing file recieved
         input_file_text = input_name.getvalue().decode()
         input_file = input_file_text.replace(",",".").replace(';', '\t')
-        st.write(input_file[0])
+        st.write(input_file)
         
         
         #Storing parameters in convinient way
