e706e5345f409484d7050602ab585374f00f8f1d
AlexP04 <84740734+AlexP04@users.noreply.github.com>
2022-11-20 01:05:10 +0200

    Add files via upload

--- tool.py
+++ tool.py
@@ -12,7 +12,6 @@ def get_err(params):
     #Using Solve class defined - get full info about errors
     params_new = params[-1].copy()
     params_new['degrees'] = [*(params[:-1])]
-    raise Exception("FF: "+str(params_new['degrees']))
     solver = Solve(params_new)
     solver.run()
     
